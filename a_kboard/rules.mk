d6a3fc3481bab4679013fa4ecc6de19ca8c2660d
UnholyXD <unholyxd@gmail.com>
2025-06-07 12:17:30 -0300

    atualizado teclado

---
+++ a_kboard/rules.mk
@@ -0,0 +1,8 @@
+# Enable VIAL support
+VIAL_ENABLE = yes
+
+# If you want to enable debug options in VIAL (optional)
+# VIAL_ENABLE_RAW = yes
+
+# Enable general features that might be needed, if not already in keyboard.json's "features"
+# ENCODER_ENABLE = yes # Already in keyboard.json features, but good to have if you move it
