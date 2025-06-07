0040c5229f908feb0c4e91464f9014a0735a30a9
UnholyXD <unholyxd@gmail.com>
2025-06-07 13:57:23 -0300

    movido arquivos

--- a_kboard/keymaps/vial/config.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x6B, 0xD7, 0x4E, 0x4E, 0x89, 0xC3, 0xE4, 0xBA}
-#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
-#define VIAL_UNLOCK_COMBO_COLS {0, 1}
